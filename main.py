dd9e648729f10e2b4ddb68902d3d5cf9a82d113f
Salena Sarella <salenasa@pr-c8.nsk.21-school.ru>
2023-05-07 17:10:01 +0700

    add v4

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import pandas as pd
+
+
 class Car:
     def __init__(self, brand, model, release, price):
         self.brand = brand
         self.model = model
         self.release = release
         self.price = price
+
 
 car1 = Car('BMW', 'M3 F80', 2014, 3550.00)
 car2 = Car('Ferrari', '488 GTB', 2015, 20700.00)
@@ -20,7 +24,7 @@ while True:
     print('2 - Выход из программы.')
     print('3 - Добавление нового объекта')
     print('4 - Удаление объекта')
-    
+
     print('5 - Сортировка по свойству')
     print('6 - Фильтрация')
     print('7 - Преобразование')
@@ -31,17 +35,29 @@ while True:
         continue
 
     if v == '1':
-        for i in carArray:
-            print("Марка автомобиля: {0}, модель: {1}, год выпуска: {2}, цена: {3} тыс. руб.".format(i.brand, i.model, i.release, i.price))
+        if (len(carArray) != 0):
+            brands = [i.brand for i in carArray]
+            models = [i.model for i in carArray]
+            releases = [i.release for i in carArray]
+            prices = [(str(i.price) + ' тыс. руб.') for i in carArray]
+            df = pd.DataFrame({"Марка автомобиля": brands,
+                              "Модель": models, "Год выпуска": releases, "Цена": prices})
+            print(df)
+
+        else:
+            print("Автосалон пуст.")
     elif v == '2':
         break
     elif v == '3':
-        brand = input("Введите марку авто: ")
-        model = input("Введите модель: ")
-        release = int(input("Введите год выпуска: "))
-        price = float(input("Введите цену в тыс. руб.: "))
-        newCar = Car(brand, model, release, price)
-        carArray.append(newCar)
+        if (len(carArray) < 12):
+            brand = input("Введите марку авто: ")
+            model = input("Введите модель: ")
+            release = int(input("Введите год выпуска: "))
+            price = float(input("Введите цену в тыс. руб.: "))
+            newCar = Car(brand, model, release, price)
+            carArray.append(newCar)
+        else:
+            print("Автосалон полон.")
     elif v == '4':
         choice = int(input("Введите номер объекта, который хотите удалить: "))
         carArray.pop(choice-1)
@@ -65,10 +81,10 @@ while True:
         year = int(input('Введите год выпуска: '))
         for i in carArray:
             if i.release >= year:
-                print("Марка автомобиля: {0}, модель: {1}, год выпуска: {2}, цена: {3} тыс. руб.".format(i.brand, i.model, i.release, i.price))
+                print("Марка автомобиля: {0}, модель: {1}, год выпуска: {2}, цена: {3} тыс. руб.".format(
+                    i.brand, i.model, i.release, i.price))
     elif v == '7':
         percent = int(input('Введите процент скидки: '))
         for i in carArray:
             i.price = i.price - i.price * percent/100
     print()
-    
